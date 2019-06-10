a4e7aabf4f92cf8f6e2a0e051a744c69f4aa2386
Mike Miller <michael.miller1@kellogg.northwestern.edu>
2019-06-10 17:18:18 +0000

    Front page
    updated front pages, going to start testing with getting pages

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -11,5 +11,34 @@
 #
 
 class Restaurant < ApplicationRecord
+    
+    def num_reviews
+        
+        num=0
+        Item.where("restaurant_id = ?", self.id).each do |item|
+         num = num + item.num_reviews
+        end
+        
+        return num
+        
+    end
+    
+    
+    def average_review
+        
+        if self.num_reviews==0
+            return 0
+        else
+            
+            a = 0
+                Item.where("restaurant_id =? ", self.id).each do |item|
+                a = a + item.average_review * item.num_reviews
+            end
+        
+            average = a/self.num_reviews
+            return average.round(2)
+        end
+    end
+    
     has_many :items, :dependent => :destroy
 end
